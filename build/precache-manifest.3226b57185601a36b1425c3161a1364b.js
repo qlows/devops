24d8edeb5090b9253dbc4a8104165aa565f8c3d5
qlows <79415956+qlows@users.noreply.github.com>
2022-10-31 21:11:40 -0400

    Hosting SPA GitHub Pages

---
+++ build/precache-manifest.3226b57185601a36b1425c3161a1364b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "211372a5b7e87f14f5cb",
+    "url": "/static/css/main.2e1f063f.chunk.css"
+  },
+  {
+    "revision": "211372a5b7e87f14f5cb",
+    "url": "/static/js/main.211372a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "f121384b6b260dfbad09037afb46e060",
+    "url": "/index.html"
+  }
+];
